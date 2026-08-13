3bd10e011141794f5baa5cba8056707060339eff
FaviohuamanVG <favio.huaman@vallegrande.edu.pe>
2026-08-13 17:27:40 -0500

    Reorganiza estructura de carpetas por capitulos y agrega avances

---
+++ Capitulo_2_Operators_and_Statements/src/main/java/com/app/Using_Additional_Binary_Operators/Assignment_Operators/Operators.java
@@ -0,0 +1,30 @@
+package com.app.Using_Additional_Binary_Operators.Assignment_Operators;
+
+public class Operators {
+    public static void main(String[] args) {
+        /*
+        * Operador de asignación
+        * 
+        * 
+        */
+        // el mas basico -->  =
+        int x = 1;
+        // Guarda el valor 1 dentro de la variable x.
+
+        // No se puede asignar un valor double a una variable int
+
+        /* 
+        int x = 1.0; // DOES NOT COMPILE
+        // Porque 1.0 es considerado un double.
+        short y = 1921222; // DOES NOT COMPILE
+        // 1921222 es demasiado grande para un short.
+        int z = 9f; // DOES NOT COMPILE
+        // Este número es float.
+        long t = 192301398193810323; // DOES NOT COMPILE
+        // Ese número es demasiado grande para int.
+        */
+
+        System.out.println("Using Additional Binary Operators - Assignment Operators");
+        
+    }
+}
---
+++ Capitulo_2_Operators_and_Statements/src/main/java/com/app/Using_Additional_Binary_Operators/Logical_Operators/OperatorsLogic.java
@@ -0,0 +1,48 @@
+package com.app.Using_Additional_Binary_Operators.Logical_Operators;
+
+public class OperatorsLogic {
+    public static void main(String[] args) {
+        /* Operadores lógicos 
+            Sirven para combinar o evaluar condiciones booleanas (true o false).
+            && — AND lógico ->  Solo da true cuando ambos valores son true.
+            || — OR lógico ->  Da false únicamente cuando ambos son false.
+        */
+
+        /*boolean x = true;
+        boolean y = false;
+        */
+        // x && y -- ¿x Y y son verdaderos?
+
+        // boolean c = true || (y < 4);  // En un OR (||), si uno de los lados es true, el resultado siempre será true.
+
+
+        /*if(a != null && a.getValue() < 5) {
+            // Do something
+        }*/
+
+        /*
+         * Como && necesita que AMBAS condiciones sean true,
+        * Java ya sabe que el resultado será false.
+        * Por eso NO ejecuta a.getValue().
+        * Esto evita un NullPointerException.
+        */
+
+        /*if(a != null && a.getValue() < 5) {
+            // Do something
+        }*/
+
+        /*
+        * Si a es null:
+        * a != null → false
+        * a.getValue() → intenta ejecutarse
+        * Esto provoca NullPointerException.
+        */
+
+        int x = 6;
+        boolean y = (x >= 6) || (++x <= 7);
+        System.out.println(x);
+
+
+
+    }
+}
